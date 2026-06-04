a8b306be990f480828e935a95152da664a40c5db
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-06-04 13:23:58 +0500

    Add files via upload

---
+++ lec-01.js
@@ -0,0 +1,40 @@
+// data types;
+// 1- string that is written in inverted commas , alphabet and numbers
+console.log("we are learning js basics");
+// 2- for more specificity numbers are categorized as integers
+console.log(7361712);
+// 3- double/float as decimal numbers
+console.log(7.361712);
+// 4- boolean as true or false
+console.log(true);
+
+
+
+// Variables are used to store data in the form of data types.
+//Variables are made by two keywords i.e let or const
+
+const name = "ali ahmad";
+// name = "jawad ali";
+console.log(name);
+
+// Cases to write variables
+// 1- PascalCase
+// 2- camelCase
+// 3- snake_case
+// 4- kebab-case
+
+// 1- PascalCase
+let UserName = "Aiman";
+console.log("The user name is",UserName,".");
+
+// 2- camelCase
+let clientName = "Moiz";
+console.log("The client name is",clientName);
+
+// 3- snake_case
+let student_name = "Zamin";
+console.log("The student name is",student_name);
+
+// 4- kebab-case
+// let my-variable-name = "Zamin";
+// console.log("The student name is",owner-name);
