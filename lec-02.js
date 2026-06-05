75f3fca3535c22a1c227b09e6301a074f0533f05
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-06-05 13:10:48 +0500

    operators

---
+++ lec-02.js
@@ -0,0 +1,29 @@
+console.log("Operators");
+console.log("Arithmetic Operators");
+// arithmetic operators(+,-,*,/,%)
+let numberOne = 55;
+let numberTwo = 90;
+// console.log(numberOne+numberTwo);
+// console.log(numberOne-numberTwo);
+
+let sumOfNumbers = numberOne+numberTwo;
+console.log(sumOfNumbers);
+
+let subtractNumbers = numberTwo-numberOne;
+console.log(subtractNumbers);
+
+let multiplyNumbers = numberTwo*numberOne;
+console.log(multiplyNumbers);
+
+let divideNumbers = numberTwo/numberOne;
+console.log(divideNumbers);
+
+let modulusOfNumbers = numberTwo%numberOne;
+console.log(modulusOfNumbers);
+
+console.log("Assignment Operators");
+// assignment operators(+=,-=,*=,/=,%=)
+let m = 56;
+console.log(m);
+m %= 90;
+console.log(m);
