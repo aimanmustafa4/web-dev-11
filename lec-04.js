26e7fbcdda2816bdb8bd42297486ba9870864116
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-06-19 09:43:41 +0500

    arrays and switch case

---
+++ lec-04.js
@@ -0,0 +1,44 @@
+console.log("conditions and switch case");
+
+// let dayToday = prompt("what is the day today");
+// console.log(dayToday);
+
+// switch (dayToday) {
+//     case "Monday":
+//         console.log("today is monday");
+//         break;
+//     case "Tuesday":
+//         console.log("today is Tuesday");
+//         break;
+//     case "Wednesday":
+//         console.log("today is Wednesday");
+//         break;
+//     case "Thursday":
+//         console.log("today is Thursday");
+//         break;
+//     default:
+//         console.log("its weekend");
+//         break;
+// }
+
+
+// Arrays is a list/collection of data .
+// array is of two types
+/**
+ * arrays are of two types 
+ * 1-  simple array => []
+ * 2- associative array => key value pair
+ */
+
+let arrayNames = ['iqra','asif','mateen','umair','umar','hasseb'];
+console.log(arrayNames);
+console.log(arrayNames[5]);
+
+
+let personDetails = {
+    userName : "Yasir",
+    userAge : 23,
+    userCity : "bwp"
+}
+console.log(personDetails.userCity);
+
