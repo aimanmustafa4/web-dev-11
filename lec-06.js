123e5354394c402bc68c58544f9de1eabdfde3fe
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-06-29 10:21:30 +0500

    FUNCTION

---
+++ lec-06.js
@@ -0,0 +1,20 @@
+console.log("Arrow Function");
+
+function sum(m,n){
+    console.log(m+n);
+}
+sum(7,8);
+ 
+
+
+
+const add = (a , b) => {
+    return a+b;
+}
+console.log(add(3,6));
+
+
+const square = n => {
+    return n*n;
+}
+console.log(square(9));
