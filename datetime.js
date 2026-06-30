f886ffa03621235643a44b445bf9de5aa2625235
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-06-30 09:57:59 +0500

    math and datetime

---
+++ datetime.js
@@ -0,0 +1,29 @@
+// const date = new Date();
+// console.log(date);
+
+// let date1 = new Date("2026-01-21");
+// let date2 = new Date("2003-01-21");
+  
+
+ 
+
+const date = new Date();
+console.log(date);
+// const year = date.getFullYear();
+// console.log(year);
+// const month=date.getMonth();
+// console.log(month);
+// const day = date.getDay();
+// console.log(day);
+// const hours = date.getHours();
+// console.log(hours);
+
+date.setFullYear(2003);
+
+
+date.setMonth(2);
+date.setDate(2);
+// date.setDay(2);
+// date.setFullYear(2003);
+console.log(date);
+
---
+++ math.js
@@ -0,0 +1,21 @@
+console.log("math functions");
+let w = -9.99;
+let x = 81;
+let y = 567;
+let z = -778;
+
+
+// x = Math.round(x);
+//  x = Math.ceil(x);greater shift
+//  x = Math.floor(x);lower shift
+//  x = Math.pow(x, 4);
+//  x = Math.sqrt(x);
+//  x = Math.cbrt(x);
+//  z = Math.abs(z);
+// x = Math.random();
+ 
+// console.log(x);
+
+// let maximum = Math.max(w,x,y,z);
+let minimum = Math.min(w,x,y,z);
+console.log(minimum);
