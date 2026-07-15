43d9807cb8162778fdbce0c59f1fb2dcd8f156ac
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-07-15 09:57:11 +0500

    php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+echo "Hello World";
+
+$userName = "John Doe";
+$userAge = 30;
+echo "<br>";
+echo "<hr>";
+echo "User Name: " . $userName ;
+echo "<br>";
+echo "User Age: " . $userAge ;
+echo "<br>";
+// operators
+$a = 21;
+$b = 45;
+$sum = $a+$b;
+$difference = $a-$b;
+echo "the sum of a and b is $sum";
+echo "the difference of a and b is $difference";
+
+
+
+?>
